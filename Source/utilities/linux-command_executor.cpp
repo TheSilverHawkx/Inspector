b27fd34455cfc7f2613535ea7483ae53e7e22c29
Omer Roth <omerroth@pop-os.localdomain>
2020-10-07 16:26:43 +0300

    working child process without pipe

--- Source/utilities/linux-command_executor.cpp
+++ Source/utilities/linux-command_executor.cpp
@@ -1,56 +1,121 @@
 #include "linux-command_executor.h"
 
 std::tuple<std::string,std::string,int>  execute_commnad(const char* command) {
-
-    // [0] - Child-side of pipe
-    // [1] - Parent-side of pipe
+    int pipefd[2];
     int stdout_pipe[2];
     int stderr_pipe[2];
 
-    if (pipe(stdout_pipe))
+    pid_t child_pid {};
+    int status {};
+    char buf;
+    
+    if (pipe(pipefd) == -1){
+        perror("pipe");
+        return {"","pipe",EXIT_FAILURE};
+    }
+    if (pipe(stdout_pipe) == -1){
+        perror("out pipe");
+        return {"","pipe",EXIT_FAILURE};
+    }
+
+    if (pipe(stderr_pipe) == -1){
+        perror("err pipe");
+        return {"","pipe",EXIT_FAILURE};
+    }
+
+    child_pid = fork();
+    if (child_pid == -1) {
+        close(pipefd[0]);
+        close(pipefd[1]);
+        close(stdout_pipe[0]);
+        close(stdout_pipe[0]);
+        return {"","error forking processs",-1};
+    }
+    else if (child_pid == 0) { // Child
+        close(pipefd[0]); // Close unsed write end -> // Close unused read end
+
+        
+        write(pipefd[1], "yeey",1);
+        write(pipefd[1], "\n",1);
+        close(pipefd[1]);
+
+        _exit(0);
+    }
+    else {
+        close(pipefd[1]); // Close unsed read end
+        do {
+            if (waitpid(child_pid,&status,0) == -1){
+                return {"","error on waitpid",-1};
+            }
+
+            if (WIFEXITED(status)){
+                while (read(pipefd[0],&buf,1) >0){
+                    write(STDOUT_FILENO,&buf,1);
+                }
+                close(pipefd[0]); // Reader will get EOF
+                printf("exited, status=%d\n",WEXITSTATUS(status));
+            }
+
+        } while (!WIFEXITED(status));
+        return {"exited good","",0};
+    }
+
+
+/////*
+    // [0] - read
+    // [1] - write
+    
+
+    /*if (pipe(stdout_pipe))
     {
         return {"","Could not open stdout pipe",-1};
     }
 
-    if (pipe(stderr_pipe)) {
+    /*if (pipe(stderr_pipe)) {
         close(stdout_pipe[0]);
         close(stdout_pipe[1]);
         return {"","Could not open stderr pipe", -1};
     }
 
-    pid_t pID = fork();
-    switch (pID){
+    pid_t pid = fork();
+    switch (pid){
         case -1: // Error while forking
-            close(stdout_pipe[0]);
-            close(stdout_pipe[1]);
-            close(stderr_pipe[0]);
-            close(stderr_pipe[1]);
+            //close(stdout_pipe[0]);
+            //close(stdout_pipe[1]);
+            //close(stderr_pipe[0]);
+            //close(stderr_pipe[1]);
             return {"","Could not fork process", -1};
 
         case 0: // Child Process
-            close(stdout_pipe[0]);
-            close(stderr_pipe[0]);
+            //dup2(stdout_pipe[1],STDOUT_FILENO);
+            //close(stdout_pipe[0]);
+            //close(stdout_pipe[1]);
+            
+            //close(stderr_pipe[0]);
 
-            dup2(stdout_pipe[1],1);
-            dup2(stderr_pipe[1],2);
+
+            //dup2(stderr_pipe[1],2);
             execl("/bin/bash",command,NULL);
-            return {"","",-1};
+            exit(0);
 
         default: // Parent Process
-            close(stdout_pipe[1]);
-            close(stderr_pipe[1]);
+            //close(stdout_pipe[1]);
+            //close(stderr_pipe[1]);
 
-            char stdout_buffer[100];
+            /*char stdout_buffer[100];
             char stderr_buffer[100];
             int stdout_count;
             int stderr_count;
 
-            close(stdout_pipe[0]);
-            close(stderr_pipe[0]);
 
-            stdout_count = read(stdout_pipe[1],stdout_buffer,sizeof(stdout_buffer)-1);
-            stderr_count = read(stderr_pipe[1],stderr_buffer,sizeof(stderr_buffer)-1);
+            int status;
+            wait(&status);
+            //stdout_count = read(stdout_pipe[1],stdout_buffer,sizeof(stdout_buffer)-1);
+            //stderr_count = read(stderr_pipe[1],stderr_buffer,sizeof(stderr_buffer)-1);
             
+            //close(stdout_pipe[0]);
+            //close(stderr_pipe[0]);
+            /*
             if (stdout_count >= 0) {
                 stdout_buffer[stdout_count] = 0;
                 return {stdout_buffer,stderr_buffer,stdout_count};
