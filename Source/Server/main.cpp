fdc34429569cc44e920be1d1cf5f55177ce2860a
omer <omer.roth95@gmail.com>
2021-04-21 15:25:19 +0300

    added agent db connector and tested

--- Source/Server/main.cpp
+++ Source/Server/main.cpp
@@ -4,10 +4,13 @@
     #include "..\utilities\condition_parser.h"
     #include "..\Utilities\MonitorBlockWorkflow\all_workflows.h"
     #include <cstdio>
+    #include "..\Utilities\DBConnector\Agent\db_connector.h"
 #else
     #include "../../include/rapidjson/document.h"
     #include "../Monitor-Blocks/all_monitor_blocks.h"
     #include "../Utilities/MonitorBlockWorkflow/all_workflows.h"
+    #include "../Utilities/DBConnector/Agent/db_connector.h"
+
 #endif
 
 #include <stdexcept>
@@ -18,11 +21,16 @@
 
 namespace fs = std::filesystem;
 
+static DBConnector* db_con;
 
 int main() {
     const std::string work_directory_path = fs::current_path().string();
     fs::create_directory("workdir");
     fs::permissions("workdir",fs::perms::others_all, fs::perm_options::remove);
+
+    db_con = new DBConnector((fs::current_path() / "workdir\\agent.db").string().c_str());
+
+    db_con->get("SELECT * FROM Monitors");
 
     std::cout << "Running in main.cpp" << std::endl;
 
